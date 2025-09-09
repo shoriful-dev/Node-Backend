8ecdb6cca06978c1be01e6dce8317a38a6f033c7
shoriful-dev <cmshohelrana50@gmail.com>
2025-09-09 10:18:28 +0600

    install slugify

---
+++ src/models/category.model.js
@@ -0,0 +1,54 @@
+// const mongoose = require('mongoose');
+// const slugify = require('slugify');
+// const { customError } = require('../../utils/customError');
+// const categorySchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     image: {
+//       type: String,
+//     },
+//     slug: {
+//       type: String,
+//     },
+//     subCategory: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'SubCategory',
+//       },
+//     ],
+//     isActive: {
+//       type: Boolean,
+//       default: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// // make a slug using name
+// categorySchema.pre('save', function (next) {
+//   if (this.isModified('name')) {
+//     this.slug = slugify(this.name, {
+//       replacement: '-',
+//       remove: undefined,
+//       lower: true,
+//       strict: false,
+//       trim: true,
+//     });
+//   }
+//   next();
+// });
+
+// // check is already exist slug or not
+// categorySchema.pre('save', async function (next) {
+//   const slug = await this.constructor.findOne({ slug: this.slug });
+//   if (slug && slug._id.toString() !== this._id.toString()) {
+//     throw new customError(401, 'Category Name already exist');
+//   }
+//   next();
+// });
+
+// module.exports = mongoose.models.Category || mongoose.model('Category', categorySchema);
