ade46f873857d496f6f333e81e9cd22b28f950f3
mdwasim2 <wasimmahamodraza@gmail.com>
2025-11-25 19:50:40 +0600

    add subCategories

--- src/middleware/authGuard.middleware.js
+++ src/middleware/authGuard.middleware.js
@@ -4,18 +4,17 @@ const jwt = require("jsonwebtoken");
 const { customError } = require("../../utils/customError");
 
 exports.authguard = async (req, res, next) => {
-  const accesToken = req?.headers?.authorization?.replace("Bearer ", "");
+  const accesToken = req?.headers?.authorization?.replace("Bearer ", " ").trim();
 
   try {
     if (!accesToken) {
       throw new customError(401, "No token provided!");
     }
-
     let tokenValue;
     try {
-      tokenValue = jwt.verify(accesToken, process.env.ACCESSTOKEN_SECRET);
+      tokenValue =  jwt.verify(accesToken, "da253bed35712e7a");
     } catch (err) {
-      // JWT error captured here (expired, malformed, etc.)
+      console.log(" tokenValue catch", err);
       throw new customError(410, "Token invalid or expired");
     }
 
@@ -25,6 +24,7 @@ exports.authguard = async (req, res, next) => {
     req.user = userinfo;
     next();
   } catch (error) {
+    console.log(error);
     next(error); // send error to your global error handler
   }
 };
